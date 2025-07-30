8bd2c643d12870efd05ccc2c7e8aa481ee246977
Johnnykeysofficial <johnnykeysofficiall@gmail.com>
2025-07-30 15:13:46 +0100

    updating psuedocode

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,3 @@
-//My Pseudocode 
-//firstly get a UI to work with, then delare 3 three buttons (i.e rock, paper, scissors)
-//after declaring the buttons, 
-// assign playerChoice to the button value,
-//assign computerChoice to a random value i.e (rock, paper, scissors);
-//after the assignment, 
-//compare their choices according to the game rules for final result i.e
-// if playerChoice ==== computerChoice then return draw also update the Draw score on the UI
-//else if playerChoice === rock then computerChoice === scissors return Win also update the win score on the UI
-//else if playerChoice === scissors then computerChoice === paper return Win also update the win score on the UI
-// else if playerChoice === paper then computerChoice === rock return Win also update the win score on the UI
-//else return lose also update the lose score on the UI.
-//END..
 
 //Here am declaring all the varriables i need 
 const buttons = document.querySelectorAll('.btn');
